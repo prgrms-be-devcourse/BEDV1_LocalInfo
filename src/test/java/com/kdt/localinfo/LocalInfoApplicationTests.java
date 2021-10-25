f48e6dde0837c37e3964ad54cd843ddb4bf7100f
suhyunsim <cnc1106@naver.com>
2021-10-25 21:46:31 +0900

    chore: 프로젝트 초기 설정
    Issue #3

---
+++ src/test/java/com/kdt/localinfo/LocalInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.localinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
