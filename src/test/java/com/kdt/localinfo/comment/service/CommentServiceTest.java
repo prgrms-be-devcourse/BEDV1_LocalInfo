9ac580771e0b044a3b721ac0732aa20983b31894
choiyoungkwon12 <hpwh4457@naver.com>
2021-10-27 16:12:57 +0900

    test: commentservice의 save메서드 테스트 코드 작성
    commentservice의 save메서드 테스트 코드 작성 및 댓글 생성 시 필요한 데이터 생성 쿼리 작성

---
+++ src/test/java/com/kdt/localinfo/comment/service/CommentServiceTest.java
@@ -0,0 +1,51 @@
+package com.kdt.localinfo.comment.service;
+
+import com.kdt.localinfo.comment.dto.CommentSaveRequest;
+import com.kdt.localinfo.comment.entity.Comment;
+import com.kdt.localinfo.comment.repository.CommentRepository;
+import com.kdt.localinfo.post.entity.Post;
+import com.kdt.localinfo.post.repository.PostRepository;
+import com.kdt.localinfo.user.entity.User;
+import com.kdt.localinfo.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+@SpringBootTest
+@Sql(scripts = {"classpath:data.sql"})
+class CommentServiceTest {
+
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @Transactional
+    void saveTest() {
+        User user = userRepository.findById(1L).orElseThrow();
+        Post post = postRepository.findById(1L).orElseThrow();
+
+        CommentSaveRequest commentSaveRequest = new CommentSaveRequest(user.getId(), post.getId(), "안녕하세요 댓글입니다.");
+        Long commentId = commentService.save(commentSaveRequest);
+
+        Comment comment = commentRepository.findById(commentId).orElseThrow();
+
+        assertThat(comment.getId(), is(1L));
+        assertThat(comment.getContents(), is("안녕하세요 댓글입니다."));
+        assertThat(comment.getParentId(), is(0L));
+        assertThat(comment.getDeletedAt(), is(nullValue()));
+        assertThat(comment.getUser(), is(user));
+        assertThat(comment.getPost(), is(post));
+    }
+}
