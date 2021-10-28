b98cf557142e5f2fae420ad66fede4ac5b3a66ba
suhyunsim <cnc1106@naver.com>
2021-10-28 19:30:21 +0900

    feat: 예외처리를 위한 EntityModel과 ErrorSerializer 구현
    - Spring HATEOAS 적용
    
    Issue #8

---
+++ src/main/java/com/kdt/localinfo/commons/ErrorsSerializer.java
@@ -0,0 +1,18 @@
+package com.kdt.localinfo.commons;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeFieldName("errors");
+        gen.writeStartArray();
+    }
+}
