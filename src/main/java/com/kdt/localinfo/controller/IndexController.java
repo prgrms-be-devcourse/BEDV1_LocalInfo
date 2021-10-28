946920d0b139cd1613f4482909684f48e91ef4c7
suhyunsim <cnc1106@naver.com>
2021-10-29 02:10:59 +0900

    refactor: IndexController에 GetMapping 엔드포인트 수정
    Issue #8

--- src/main/java/com/kdt/localinfo/controller/IndexController.java
+++ src/main/java/com/kdt/localinfo/controller/IndexController.java
@@ -10,7 +10,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RestController
 public class IndexController {
 
-    @GetMapping("/api")
+    @GetMapping("/api/index")
     public RepresentationModel index() {
         var index = new RepresentationModel();
         index.add(linkTo(UserController.class).withRel("users"));
