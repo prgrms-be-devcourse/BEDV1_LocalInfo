d57bb3fb75eea0a2f6ab91dc68431bfd7606e567
eonju.dev <eonju.dev@gmail.com>
2021-11-03 20:29:41 +0900

    fix : @ActiveProfiles("test") 추가 Issue #46

--- src/test/java/com/kdt/localinfo/post/service/PostServiceTest.java
+++ src/test/java/com/kdt/localinfo/post/service/PostServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
@@ -21,6 +22,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 class PostServiceTest {
