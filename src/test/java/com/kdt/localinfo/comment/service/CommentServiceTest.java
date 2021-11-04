dd51453f6b18daf43e9a43e7456ccc4c012ec20b
choiyoungkwon12 <hpwh4457@naver.com>
2021-11-04 16:26:18 +0900

    test: 댓글 생성 시 사진 업로드 기능 테스트
    Issue: #52

--- src/test/java/com/kdt/localinfo/comment/service/CommentServiceTest.java
+++ src/test/java/com/kdt/localinfo/comment/service/CommentServiceTest.java
@@ -1,5 +1,6 @@
 package com.kdt.localinfo.comment.service;
 
+import com.kdt.localinfo.aws.service.AwsS3Service;
 import com.kdt.localinfo.comment.converter.CommentConverter;
 import com.kdt.localinfo.comment.dto.CommentDepth;
 import com.kdt.localinfo.comment.dto.CommentResponse;
@@ -7,19 +8,28 @@ import com.kdt.localinfo.comment.dto.CommentSaveRequest;
 import com.kdt.localinfo.comment.entity.Comment;
 import com.kdt.localinfo.comment.repository.CommentRepository;
 import com.kdt.localinfo.common.TestEntityFactory;
+import com.kdt.localinfo.photo.CommentPhoto;
+import com.kdt.localinfo.photo.CommentPhotoRepository;
 import com.kdt.localinfo.post.entity.Post;
 import com.kdt.localinfo.post.repository.PostRepository;
 import com.kdt.localinfo.user.entity.User;
 import com.kdt.localinfo.user.repository.UserRepository;
 import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +37,11 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
 
@@ -44,19 +55,23 @@ class CommentServiceTest {
     private PostRepository postRepository;
     @Mock
     private CommentConverter commentConverter;
+    @Mock
+    private AwsS3Service s3Uploader;
+    @Mock
+    private CommentPhotoRepository commentPhotoRepository;
 
     @Test
     @Transactional
     @DisplayName("댓글 생성")
-    void saveTest() throws NotFoundException {
+    void saveTest() throws NotFoundException, IOException {
         // GIVEN
+        String url = "2544a8cf-b522-48f4-915a-6425018c5957-test2.jpg";
+
         Comment comment = TestEntityFactory.commentBuilder().build();
         User user = comment.getUser();
         Post post = comment.getPost();
 
         CommentSaveRequest commentSaveRequest = new CommentSaveRequest(user.getId(), "안녕하세요 댓글입니다.");
-
-        Comment expectComment = commentConverter.converterToComment(commentSaveRequest, user, post);
 
         CommentResponse expectCommentResponse = new CommentResponse(comment.getId(),
                 "안녕하세요 댓글입니다.",
@@ -64,15 +79,29 @@ class CommentServiceTest {
                 LocalDateTime.now(),
                 user.getRegion().getNeighborhood(),
                 null,
-                checkedDepth(comment.getParentId()));
+                checkedDepth(comment.getParentId()),
+                List.of(url));
 
+        Path directoryPath = Paths.get("comment-photo");
+        File imageFile = new File(directoryPath + "\\test.jpg");
+        MockMultipartFile firstFile = new MockMultipartFile("images", "test.jpg", null, Files.readAllBytes(imageFile.toPath()));
+
+        CommentPhoto commentPhoto = new CommentPhoto(url, comment);
+        List<CommentPhoto> commentPhotos = new ArrayList<>();
+        commentPhotos.add(commentPhoto);
+
+        given(commentRepository.save(comment)).willReturn(comment);
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
         given(postRepository.findById(post.getId())).willReturn(Optional.of(post));
-        given(commentConverter.converterToComment(commentSaveRequest, user, post)).willReturn(expectComment);
-        given(commentConverter.converterToCommentResponse(expectComment)).willReturn(expectCommentResponse);
+        given(commentConverter.converterToComment(commentSaveRequest, user, post)).willReturn(comment);
+        given(commentConverter.converterToCommentPhoto(comment, url)).willReturn(commentPhoto);
+        given(commentRepository.save(comment)).willReturn(comment);
+        given(commentPhotoRepository.saveAll(commentPhotos)).willReturn(commentPhotos);
+        given(commentConverter.converterToCommentResponse(comment,List.of(url))).willReturn(expectCommentResponse);
+        given(s3Uploader.upload(firstFile,"comment-photo")).willReturn(url);
 
         // WHEN
-        CommentResponse commentResponse = commentService.save(commentSaveRequest, post.getId());
+        CommentResponse commentResponse = commentService.save(commentSaveRequest, post.getId(), List.of(firstFile));
 
         // THEN
         assertThat(commentResponse.getId(), is(expectCommentResponse.getId()));
@@ -81,6 +110,7 @@ class CommentServiceTest {
         assertThat(commentResponse.getDepth(), is(expectCommentResponse.getDepth()));
         assertThat(commentResponse.getRegion(), is(expectCommentResponse.getRegion()));
         assertThat(commentResponse.getNickName(), is(expectCommentResponse.getNickName()));
+        assertThat(commentResponse.getUrls(), is(expectCommentResponse.getUrls()));
     }
 
     @Test
