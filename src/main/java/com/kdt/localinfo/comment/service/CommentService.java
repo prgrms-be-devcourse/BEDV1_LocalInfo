8ef24c5897d8ec1d620e322d9e5bf006944432fb
choiyoungkwon12 <hpwh4457@naver.com>
2021-11-04 16:19:18 +0900

    feat: 댓글 생성 시 사진 업로드 기능 구현
    Issue: #52

--- src/main/java/com/kdt/localinfo/comment/service/CommentService.java
+++ src/main/java/com/kdt/localinfo/comment/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.kdt.localinfo.comment.service;
 
+import com.kdt.localinfo.aws.service.AwsS3Service;
 import com.kdt.localinfo.comment.converter.CommentConverter;
 import com.kdt.localinfo.comment.dto.CommentResponse;
 import com.kdt.localinfo.comment.dto.CommentSaveRequest;
 import com.kdt.localinfo.comment.entity.Comment;
 import com.kdt.localinfo.comment.repository.CommentRepository;
+import com.kdt.localinfo.photo.CommentPhoto;
+import com.kdt.localinfo.photo.CommentPhotoRepository;
 import com.kdt.localinfo.post.entity.Post;
 import com.kdt.localinfo.post.repository.PostRepository;
 import com.kdt.localinfo.user.entity.User;
@@ -13,7 +16,10 @@ import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,16 +31,26 @@ public class CommentService {
     private final CommentConverter commentConverter;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final CommentPhotoRepository commentPhotoRepository;
+    private final AwsS3Service s3Uploader;
 
-    public CommentService(CommentRepository commentRepository, CommentConverter commentConverter, UserRepository userRepository, PostRepository postRepository) {
+    public CommentService(CommentRepository commentRepository,
+                          CommentConverter commentConverter,
+                          UserRepository userRepository,
+                          PostRepository postRepository,
+                          CommentPhotoRepository commentPhotoRepository,
+                          AwsS3Service s3Uploader) {
         this.commentRepository = commentRepository;
         this.commentConverter = commentConverter;
         this.userRepository = userRepository;
         this.postRepository = postRepository;
+        this.commentPhotoRepository = commentPhotoRepository;
+        this.s3Uploader = s3Uploader;
     }
 
     @Transactional
-    public CommentResponse save(CommentSaveRequest commentSaveRequest, Long postId) throws NotFoundException {
+    public CommentResponse save(CommentSaveRequest commentSaveRequest, Long postId, List<MultipartFile> multipartFiles) throws NotFoundException, IOException {
+
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new NotFoundException("게시물에 대한 정보를 찾을 수 없습니다."));
 
@@ -46,7 +62,18 @@ public class CommentService {
 
         Comment commentEntity = commentRepository.save(comment);
 
-        return commentConverter.converterToCommentResponse(commentEntity);
+        List<String> fileUrls = fileUpload(multipartFiles);
+
+        List<CommentPhoto> commentPhotos = fileUrls.stream()
+                .map(url -> commentConverter.converterToCommentPhoto(comment, url))
+                .collect(Collectors.toList());
+
+        List<CommentPhoto> savedPhotos = commentPhotoRepository.saveAll(commentPhotos);
+
+        List<String> urls = new ArrayList<>();
+        savedPhotos.forEach(commentPhoto -> urls.add(commentPhoto.getUrl()));
+
+        return commentConverter.converterToCommentResponse(commentEntity, urls);
     }
 
     @Transactional(readOnly = true)
@@ -60,5 +87,18 @@ public class CommentService {
                 .filter(comment -> comment.getDeletedAt() == null)
                 .map(commentConverter::converterToCommentResponse)
                 .collect(Collectors.toList());
+    }
+
+    private List<String> fileUpload(List<MultipartFile> multipartFiles) throws IOException {
+        List<String> uploadUrls = new ArrayList<>();
+
+        if (multipartFiles != null) {
+            for (MultipartFile multipartFile : multipartFiles) {
+                    String upload = s3Uploader.upload(multipartFile, "comment-photo");
+                    uploadUrls.add(upload);
+            }
+        }
+
+        return uploadUrls;
     }
 }
