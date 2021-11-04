f85f2cd161288916f1dac7bab921236e3d446b43
suhyunsim <cnc1106@naver.com>
2021-11-05 03:20:38 +0900

    feat: User 수정 기능 구현
    - UserController에 PutMapping 매핑
    - UserService에 User 수정하는 메서드 구현
    
    Issue #48

--- src/main/java/com/kdt/localinfo/user/service/UserService.java
+++ src/main/java/com/kdt/localinfo/user/service/UserService.java
@@ -2,23 +2,31 @@ package com.kdt.localinfo.user.service;
 
 import com.kdt.localinfo.user.dto.UserRequest;
 import com.kdt.localinfo.user.dto.UserResponse;
+import com.kdt.localinfo.user.entity.Region;
+import com.kdt.localinfo.user.entity.Role;
 import com.kdt.localinfo.user.entity.User;
 import com.kdt.localinfo.user.repository.UserRepository;
-import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Transactional(readOnly = true)
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final ModelMapper modelMapper;
+
+    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Transactional
@@ -34,9 +42,23 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    public UserResponse getUser(Long id) throws NotFoundException {
+    public UserResponse getUser(Long id) {
         return userRepository.findById(id)
                 .map(UserResponse::new)
-                .orElseThrow(() -> new NotFoundException("해당 유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+    }
+
+    @Transactional
+    public UserResponse updateUser(Long id, UserRequest userRequest) {
+        User beforeUser = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+        modelMapper.map(userRequest, beforeUser);
+        beforeUser.getRoles().add(Role.valueOf(userRequest.getRole()));
+        beforeUser.setRegion(Region.builder()
+                .district(userRequest.getDistrict())
+                .neighborhood(userRequest.getDistrict())
+                .city(userRequest.getCity())
+                .build());
+        return new UserResponse(userRepository.save(beforeUser));
     }
 }
